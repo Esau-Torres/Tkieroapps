8505ef352961c3ddbc38df3aecac5231d6e352f6
Esau Torres <davidesau140@gmail.com>
2025-03-16 23:02:17 -0600

    Corrección de doble BrowserRouter en App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';  //importacion de bootstrap para el diseño del sitio web
 //estructura del sitio web
 import Header from './componentes/Header';
 import Footer from './componentes/Footer';
 //vistas
-import Inicio from "./Vistas/Inicio"; 
-import Contacto from "./Vistas/Contacto"; 
+import Inicio from "./Vistas/Inicio";
+import Contacto from "./Vistas/Contacto";
 import Caracteristicas from "./Vistas/Caracteristicas";
 // estilos globales
-import './App.css'; 
+import './App.css';
 
 function App() {
   return (
-    <Router>
-            <Header />
-            <main className="container mt-1" >
-                <Routes>
-                    <Route path="/" element={<Inicio />} />
-                    <Route path="/caracteristicas" element={<Caracteristicas />} />
-                    <Route path="/contacto" element={<Contacto />} />
-                </Routes>
-            </main>
-            <Footer />
-        </Router>
+
+    <>
+      <Header />
+      <main className="container mt-1" >
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/caracteristicas" element={<Caracteristicas />} />
+          <Route path="/contacto" element={<Contacto />} />
+        </Routes>
+      </main>
+      <Footer />
+    </>
   );
 }
 
